595ddab17f9aed96158652b316da3cd037060e02
Hackbright Student <ceelveetza@gmail.com>
2013-11-06 09:44:54 -0800

    Working function to parse torrent file and extract torrent info into a dictionary

--- parse_torrent.py
+++ parse_torrent.py
@@ -1,29 +1,34 @@
 import bencode
 
-def parse_torrent_file(t):
-    parsed = bencode.bdecode(t)
-    #parsed = bencode.bdecode(t)
-    #trackers = []
-    #title
-    #number_of_pieces
-    #piece_size
-    #total_size
-    #infohash
-    #encoded URL, use requests library to decode it
-
-    return parsed
-
-def main():
+def parse_torrent(t):
+    t_file = open(t).read()
+    parsed = bencode.bdecode(t_file)
+    #print "Parsed torrent: ", parsed
     torrent_info_dict = {}
-    torrent_file = "/home/user/silvia/[TorrentDownloads.me]_Principles for Open Science   presentation from Kaitlin Thaney 2009 01 05 Creative Commons CC BY.torrent"
-    t = open(torrent_file).read()
-    parsed = parse_torrent_file(t)
-    print "Parsed torrent: "
     for key in parsed.keys():
         if key == 'announce':
-            torrent_info_dict['primary tracker'] = parsed[key]
+            torrent_info_dict['Tracker_URL'] = parsed[key]
+        if key == 'title':
+            torrent_info_dict['Title'] = parsed[key]
+        if key == 'encoding':
+            torrent_info_dict['Encoding'] = parsed[key]
+        if key == 'created by':
+            torrent_info_dict['Created by'] = parsed[key]
+        if key =='info':
+            #key is another dictionary, the "info" dictionary
+            info_dict = parsed[key]
+            for info_key in info_dict.keys():
+                if info_key == 'files':
+                    torrent_info_dict['How many files'] = len(info_dict['files'])
+                if info_key == 'piece length':
+                    torrent_info_dict['Piece length'] = info_dict['piece length']
+    return torrent_info_dict
 
-    print "Torrent info: ", torrent_info_dict
+def main():
+    torrent_file = "/home/user/silvia/sample_torrents/[TorrentDownloads.me]_Principles for Open Science   presentation from Kaitlin Thaney 2009 01 05 Creative Commons CC BY.torrent"
+    torrent_info = parse_torrent(torrent_file)
 
+    for field, value in torrent_info.iteritems():
+        print field, " : ", value
 
 main()
