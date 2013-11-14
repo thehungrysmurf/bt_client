ae5d9b451ad31fa1f94dff2a38fdd21588a427d5
Hackbright Student <ceelveetza@gmail.com>
2013-11-13 16:11:42 -0800

    Created new functions to parse received messages & to assemble messages to send. Trying to have a flowing exchange of messages to the peer but so far only managed to exchange 2 messages. Still not getting Unchoked as expected

--- messages.py
+++ messages.py
@@ -1,30 +1,40 @@
 import bencode
 import parse_torrent
 from struct import *
+#from connection import InitiateConnection
 
 #introduce global variables that keep track of the steps that have been done - handshake happened, unchoke received, etc
 
 def handshake(torrent):
     #<pstrlen><pstr><reserved><info_hash><peer_id>
-    """#Old handshake:
-    pstrlen = pack('B', 19)
-    pstr = 'BitTorrent protocol'
-    reserved = pack('!8x')
-    handshake = pstrlen + pstr + reserved + torrent.info_hash + torrent.peer_id
-    return handshake"""
-
-    #New handshake: 
     pstr = 'BitTorrent protocol'
     handshake = pack("!B19s8x20s20s", len(pstr), pstr, torrent.info_hash, torrent.peer_id)
     return handshake
 
-def assemble_message(m_id):
+def assemble_message(message_name):
     prefix = "!IB"
-    if m_id == 2:
-        message = pack(prefix, 1, m_id)
-    if m_id == 5:
-        message = pack(prefix+'15s', 17, m_id, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+    message_types = {"keepalive":None, "choke": 0, "unchoke": 1, "interested":2, "not interested": 3, "have": 4, "bitfield": 5, "request": 6, "piece":7, "cancel": 8, "port": 9}
+    if message_name == "interested":
+        message = pack(prefix, 1, message_types[message_name])
+    if message_name == "bitfield":
+        what_i_have = pack('B', int('00000000'))
+        bitfield = what_i_have*7
+        message = pack(prefix+'7s', 17, message_types[message_name], bitfield)
+    if message_name == "request":
+        #request very first piece, no offset, block = 2^14
+        message = pack("!IBIII", 13, 6, 0, 0, 16384)
     return message
+
+def get_message_type(connection):
+    l = connection.receive_message(4)
+    if len(l) < 1:
+        return "Message length < 1 - keepalive or invalid message."
+    length = int(unpack("!I", l)[0])
+    m = connection.receive_message(1)
+    message_id = unpack("!B", m)[0]
+    if length > 1:
+        payload = connection.receive_message(length-1)
+    return length, message_id, payload
 
 class Message(object):
     #alternatively, create a dictionary like Zach: Messages = {'keepalive': -1, 'choke':0, 'unchoke':1, ....}
@@ -79,11 +89,4 @@ class cancel(Message):
 
 class port(Message):
     m_id = 9
-
-'''def main():
-    torrent_file = "/home/user/silvia/my_torrents_as_tracker/file_1.torrent"
-    h = handshake(torrent_file)
-    print h, len(h)
-
-main()'''
 
--- test_process.py
+++ test_process.py
@@ -6,54 +6,53 @@ from torrent import Torrent
 from connection import InitiateConnection
 import messages
 
-TORRENT_TEST_FILE = '/home/silvia/Hackbright/my_BT_client/misc/torrents/File_1.torrent'
+TORRENT_TEST_FILE = '/home/user/silvia/my_torrents_as_tracker/file_1.torrent'
 
 def main():
     t = Torrent(TORRENT_TEST_FILE)
-    print "************Metainfo: ", t.info_dict
-    print "************Piece length: ", t.piece_length
-    print "************No. of subpieces", t.no_of_subpieces
+    #print "************Metainfo: ", t.info_dict
+    #print "************Piece length: ", t.piece_length
+    #print "************No. of subpieces", t.no_of_subpieces
     #print "************List of hashes of subpieces: ", t.list_of_subpieces_hashes
-    print "************Info hash: ", t.info_hash.encode('base64')
-    c = InitiateConnection('127.0.0.1', 51413)
-    print "************Response from tracker to HTTP GET request: ", c.send_request_to_tracker(t)
+    #print "************Info hash: ", t.info_hash.encode('base64')
+    c = InitiateConnection('10.1.10.25', 51413)
+    #print "************Response from tracker to HTTP GET request: ", c.send_request_to_tracker(t)
     handshake = c.generate_handshake_to_peer(t)
-    print "************Sending handshake: ", handshake
+    #print "************Sending handshake: ", handshake
     c.connect_to_peer()
+
     c.send_message(handshake)
     received = c.receive_message(1024)
     received_unpacked = unpack("!B19s8x20s20s", received)
-    print "************Full response handshake from peer: ", received
-    print "************Response handshake from peer unpacked: ", received_unpacked
-    print "************Peer id from peer: ", received[-20:]
+    #print "************Full response handshake from peer: ", received
+    #print "************Response handshake from peer unpacked: ", received_unpacked
+    #print "************Peer id from peer: ", received[-20:]
     #print "************Info hash from peer: ", hashlib.sha1(received_unpacked[2]).hexdigest()
-    print "************Info hash from peer: ", received_unpacked[2].encode('base64')
-    interested_message = messages.assemble_message(2)
-    c.send_message(interested_message)
-    size_of_interested_message = int(unpack("!I", c.receive_message(4))[0])
-    
-    message_id = c.receive_message(1)
-    
-    if ((size_of_interested_message - 1) > 0):
-        payload = c.receive_message(size_of_interested_message - 1)
-    print "Size of interested message: %r" %size_of_interested_message
-    print "Message id: ", unpack("!B", message_id)[0]
-    print "Size of payload: ", len(payload)
-    print "Payload: %r" %payload
-    unpacked_payload = unpack("!15B", payload)
-    print "Payload unpacked: ", unpacked_payload
-    bitfield = messages.assemble_message(5)
-    print "bitfield: ", bitfield
-    c.send_message(bitfield)
-    x = c.receive_message(1024)
-    print x
+    #print "************Info hash from peer: ", received_unpacked[2].encode('base64')
 
+    #sending bitfield:
+    b = messages.assemble_message("bitfield")
+    print "sending bitfield: ", unpack("!IB7s", b)
+    c.send_message(b)
+    rb = messages.get_message_type(c)
+    print "reply: ", rb
+    print "size of payload: ", len(rb[2])
+    print "payload: %r" %rb[2]
+    unpacked_payload = unpack("!7B", rb[2])
+    print "payload unpacked: ", unpacked_payload
 
-    """request_message = pack("!IBIII", 13, 6, 0, 0, 16384)
-    c.send_message(request_message)
-    x = c.receive_message(1024)
-    print unpack('!IB', x)
-    print len(x)"""
+    #sending interested:
+    i = messages.assemble_message("interested")
+    print "sending interested: ", unpack("!IB7", i)
+    c.send_message(i)
+    print "reply: ", messages.get_message_type(c)
+
+    #sending request:
+    req = messages.assemble_message("request")
+    print "sending request: ", unpack("!IBIII", req)
+    c.send_message(req)
+    rr = messages.get_message_type(c)
+    print "reply: ", rr
 
 if __name__ == "__main__":
     main()
